3d8dac21ac7e4ef2f00b12e3b2e9bc86c4588f2a
Mohsin Zafar <heykhattak@gmail.com>
2026-01-31 22:25:30 +0500

    Feature: Page for different render type created

---
+++ app/isr-page/page.tsx
@@ -0,0 +1,9 @@
+export default function ISRPage() {
+  return (
+    <div className="flex w-full items-center flex-column min-h-screen">
+      <h1 className="text-3xl font-bold mx-auto">
+        This is an Incrementally Updated Server Side Rendered Page
+      </h1>
+    </div>
+  );
+}
---
+++ app/ssr-page/page.tsx
@@ -0,0 +1,9 @@
+export default function SSRPage() {
+  return (
+    <div className="flex w-full items-center flex-column min-h-screen">
+      <h1 className="text-3xl font-bold mx-auto">
+        This is a Server Side Rendered Page
+      </h1>
+    </div>
+  );
+}
